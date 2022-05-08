48aca7d090ecf8277cbfbd47937d088676174588
duytaovo <duytaovo@gmail.com>
2022-05-08 21:10:06 +0700

    first commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pipe6 = new System.Windows.Forms.PictureBox();
+            this.pipe5 = new System.Windows.Forms.PictureBox();
+            this.pipe4 = new System.Windows.Forms.PictureBox();
+            this.pipe3 = new System.Windows.Forms.PictureBox();
+            this.pipe2 = new System.Windows.Forms.PictureBox();
+            this.pipe1 = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pipe6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipe5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipe4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipe3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipe2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipe1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pipe6
+            // 
+            this.pipe6.BackColor = System.Drawing.Color.Transparent;
+            this.pipe6.Image = ((System.Drawing.Image)(resources.GetObject("pipe6.Image")));
+            this.pipe6.Location = new System.Drawing.Point(712, -13);
+            this.pipe6.Name = "pipe6";
+            this.pipe6.Size = new System.Drawing.Size(71, 141);
+            this.pipe6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipe6.TabIndex = 16;
+            this.pipe6.TabStop = false;
+            // 
+            // pipe5
+            // 
+            this.pipe5.BackColor = System.Drawing.Color.Transparent;
+            this.pipe5.Image = ((System.Drawing.Image)(resources.GetObject("pipe5.Image")));
+            this.pipe5.Location = new System.Drawing.Point(512, -13);
+            this.pipe5.Name = "pipe5";
+            this.pipe5.Size = new System.Drawing.Size(71, 141);
+            this.pipe5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipe5.TabIndex = 15;
+            this.pipe5.TabStop = false;
+            // 
+            // pipe4
+            // 
+            this.pipe4.BackColor = System.Drawing.Color.Transparent;
+            this.pipe4.Image = ((System.Drawing.Image)(resources.GetObject("pipe4.Image")));
+            this.pipe4.Location = new System.Drawing.Point(312, -13);
+            this.pipe4.Name = "pipe4";
+            this.pipe4.Size = new System.Drawing.Size(71, 141);
+            this.pipe4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipe4.TabIndex = 14;
+            this.pipe4.TabStop = false;
+            // 
+            // pipe3
+            // 
+            this.pipe3.BackColor = System.Drawing.Color.Transparent;
+            this.pipe3.Image = ((System.Drawing.Image)(resources.GetObject("pipe3.Image")));
+            this.pipe3.Location = new System.Drawing.Point(712, 219);
+            this.pipe3.Name = "pipe3";
+            this.pipe3.Size = new System.Drawing.Size(71, 117);
+            this.pipe3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipe3.TabIndex = 13;
+            this.pipe3.TabStop = false;
+            // 
+            // pipe2
+            // 
+            this.pipe2.BackColor = System.Drawing.Color.Transparent;
+            this.pipe2.Image = ((System.Drawing.Image)(resources.GetObject("pipe2.Image")));
+            this.pipe2.Location = new System.Drawing.Point(512, 219);
+            this.pipe2.Name = "pipe2";
+            this.pipe2.Size = new System.Drawing.Size(71, 117);
+            this.pipe2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipe2.TabIndex = 12;
+            this.pipe2.TabStop = false;
+            // 
+            // pipe1
+            // 
+            this.pipe1.BackColor = System.Drawing.Color.Transparent;
+            this.pipe1.Image = ((System.Drawing.Image)(resources.GetObject("pipe1.Image")));
+            this.pipe1.Location = new System.Drawing.Point(312, 219);
+            this.pipe1.Name = "pipe1";
+            this.pipe1.Size = new System.Drawing.Size(71, 117);
+            this.pipe1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipe1.TabIndex = 11;
+            this.pipe1.TabStop = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 200;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(25, 143);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(52, 35);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(253, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 24);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(837, 335);
+            this.Controls.Add(this.pipe6);
+            this.Controls.Add(this.pipe5);
+            this.Controls.Add(this.pipe4);
+            this.Controls.Add(this.pipe3);
+            this.Controls.Add(this.pipe2);
+            this.Controls.Add(this.pipe1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pipe6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipe5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipe4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipe3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipe2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipe1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pipe6;
+        private System.Windows.Forms.PictureBox pipe5;
+        private System.Windows.Forms.PictureBox pipe4;
+        private System.Windows.Forms.PictureBox pipe3;
+        private System.Windows.Forms.PictureBox pipe2;
+        private System.Windows.Forms.PictureBox pipe1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
